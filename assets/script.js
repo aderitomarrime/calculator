08e0a33b320850e4960bd52012f1d2513b8575ae
Adérito Marrime <153687419+aderitomarrime@users.noreply.github.com>
2025-10-18 08:21:53 +0200

    Make a function that takes two numbers and one operator
    The chosen operator will call the function responsible for the calculation, and the two numbers will be passed to that calculation function

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+let number1;
+let number2;
+let operator;
+
+
 function add(number1, number2) {
     return number1 + number2
 }
@@ -12,4 +17,24 @@ function multiply(number1, number2) {
 
 function divide(number1, number2) {
     return number1 / number2
+}
+
+function operate(number1, operator, number2) {
+    console.log(operator)
+    switch(operator) {
+        case '+' :
+            return add(number1,number2);
+        break;
+        case '-':
+            return subtract(number1,number2);
+        break;
+        case '*':
+            return multiply(number1,number2);
+        break;
+        case '/':
+            return divide(number1,number2);
+        break;
+        default:
+            return console.log("Invalid operator");
+    }
 }
